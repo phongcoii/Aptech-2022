0e9b9ff879b22c65ca460996ade9034b28609e8d
phongcoii <101492323+phongcoii@users.noreply.github.com>
2022-03-16 11:24:53 +0700

    Add files via upload

---
+++ Helloworld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("%s from tam trinh %s luv!", "\t123 sida", "with");
+    printf("mot nam co %d thang\n",12);
+    printf("Pi = %f", 3.14159265359);
+    printf("\nPi = %.2f", 3.14159265359);
+    printf("\nPi = %.4f", 3.14159265359);
+    printf("\a\a\a");
+    return 0;
+}
